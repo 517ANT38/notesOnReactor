98cfd1654f1ee173b2010106800ba6efb33673df
Anton <a.tashchilin@bk.ru>
2024-01-03 22:55:35 +0400

    add open api config and change app config

---
+++ src/main/java/com/app/testingService/configuration/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.app.testingService.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean 
+    public OpenAPI openAPI(SecurityScheme s) { 
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()         
+           .addList("Bearer Authentication")) 
+            .components(new Components().addSecuritySchemes 
+            ("Bearer Authentication", s)) 
+           .info(new Info() 
+                    .title("Notes API") 
+                    .description(""" 
+                        Default users: login:admin,password:admin,role:ADMIN;  
+                    """) 
+            ); 
+    } 
+     
+ 
+    @Bean
+    public SecurityScheme createAPIKeyScheme() { 
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP) 
+                .bearerFormat("JWT") 
+                .scheme("bearer"); 
+    }
+}
